22c2c622cc6b95af782ac5199cf6cb3b0c92d5af
distorition <85959781+distorition@users.noreply.github.com>
2021-07-25 18:22:13 +0300

    Add files via upload

---
+++ Lesson 10/Lesson 10.2/Lesson 10.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"факториал  {Factorial(num)}");
+            Console.WriteLine($"фиблначи рекурсия  { Fibonachi(num)}");
+            Console.WriteLine($"фиблначи через цкил  { FibonacciCikle(num)}");
+
+        }
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        static int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+            }
+        }
+        static int FibonacciCikle(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Lesson 10/leson 10 .1/leson 10 .1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leson_10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            int d = 0;
+            
+                
+                while (i < num)
+                {
+                    if (num % i == 0)
+                    {
+                        d++;
+                        break;
+                    }
+                    else
+                    {
+                        i++;
+                    };
+
+                }
+
+                if (d == 0)
+                {
+                    Console.WriteLine("Simple");
+                }
+                else
+                {
+                    Console.WriteLine("Not Simple");
+                }
+
+            
+        }
+    }
+}
