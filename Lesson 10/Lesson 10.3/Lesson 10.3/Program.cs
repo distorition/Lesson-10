86b51549df62d8051619da93d7201d3e8858c334
distorition <85959781+distorition@users.noreply.github.com>
2021-07-26 10:02:32 +0300

    Add files via upload

---
+++ Lesson 10/Lesson 10.3/Lesson 10.3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public static int StrangeSum(int[] inputArray)
+            {
+                int sum = 0;
+                for (int i = 0; i < inputArray.Length; i++)//O(n)
+                {
+                    for (int j = 0; j < inputArray.Length; j++)//n
+                    {
+                        for (int k = 0; k < inputArray.Length; k++)//n
+                        {
+                            int y = 0;
+
+                            if (j != 0)
+                            {
+                                y = k / j;
+                            }
+
+                            sum += inputArray[i] + i + k + j + y;
+                        }
+                    }
+                }
+
+                return sum; //O(N^3)
+            }
+
+
+        }
+    }
+}
